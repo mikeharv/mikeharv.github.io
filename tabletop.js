cf386143cc98981200953bc64740285eb3586fcd
Mike Harvey <mike.harvey@code.org>
2023-09-16 21:14:12 -0400

    delete tabletop

--- tabletop.js
+++
@@ -1,9 +0,0 @@
-function init() {
-	console.log('init');
-	Tabletop.init( { key: '1aw-lG77MkUB9volokjRMLgDNwyL_BWB3nHVhp3c1bGQ',
-		callback: function(data, tabletop) {
-			console.log(data)
-		},
-		simpleSheet: true } )
-}
-window.addEventListener(‘DOMContentLoaded’, init)
