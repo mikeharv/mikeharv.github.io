693f9748d229d823b02f4a62b69d7c62bb7fb675
Mike Harvey <mike.harvey@code.org>
2023-09-16 20:50:45 -0400

    tabletop

--- tabletop.js
+++ tabletop.js
@@ -1,8 +1,9 @@
 function init() {
-Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1aw-lG77MkUB9volokjRMLgDNwyL_BWB3nHVhp3c1bGQ/pubhtml',
-callback: function(data, tabletop) {
-console.log(data)
-},
-simpleSheet: true } )
+	console.log('init');
+	Tabletop.init( { key: '1aw-lG77MkUB9volokjRMLgDNwyL_BWB3nHVhp3c1bGQ',
+		callback: function(data, tabletop) {
+			console.log(data)
+		},
+		simpleSheet: true } )
 }
 window.addEventListener(‘DOMContentLoaded’, init)
